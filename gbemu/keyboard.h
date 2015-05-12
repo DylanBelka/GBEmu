18ecaab9e0c968f695b65b9ce4ed4fe9c74f3d32
Dylan Belka <dylan.belka@gmail.com>
2015-05-11 20:32:35 -0400

    Added keyboard input

---
+++ gbemu/keyboard.h
@@ -0,0 +1,28 @@
+#ifndef GB_KEYBOARD_H
+#define GB_KEYBOARD_H
+
+struct GBKeys
+{
+	char keys[2];
+	char colID;
+};
+
+enum KeyGroups
+{
+	p14 = 0,
+	p15
+};
+
+enum KeyCodes
+{
+	keyRight = 0x1,
+	keyA = 0x1,
+	keyLeft = 0x2,
+	keyB = 0x2,
+	keyUp = 0x4,
+	keySelect = 0x4,
+	keyDown = 0x8,
+	keyStart = 0x8
+};
+
+#endif // GB_KEYBOARD_H
