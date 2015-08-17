c7056a03a03e943e4233462d3f4332745bb892e3
DylanBelka <dylan.belka@gmail.com>
2015-08-17 13:13:41 -0400

    Added mirroring of RAM between 0xC000-0xDE00

--- gbemu/cpu.cpp
+++ gbemu/cpu.cpp
@@ -1947,7 +1947,7 @@ void CPU::wByte(addr16 addr, byte val)
 	if (isInternalMem(addr))
 	{
 		internalmem[addr] = val;
-		if (addr >= 0xC000 && addr <= 0xE000 ||
+		if (addr >= 0xC000 && addr <= 0xDE00 ||
 			addr >= 0xE000 && addr <= 0xFE00) // echo RAM
 		{
 			internalmem[addr + 0x2000] = val; // emulate mirroring of RAM
